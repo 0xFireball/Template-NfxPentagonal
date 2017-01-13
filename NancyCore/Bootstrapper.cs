0cbeb9355bd5fdd0be39c5a6ba716abb02c3e13e
0xFireball <0xFireball@outlook.com>
2017-01-12 19:13:27 -0800

    add static content mapping

--- NancyCore/Bootstrapper.cs
+++ NancyCore/Bootstrapper.cs
@@ -1,5 +1,6 @@
 using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.TinyIoc;
 
 namespace NancyCore
@@ -11,6 +12,16 @@ namespace NancyCore
             base.ApplicationStartup(container, pipelines);
 
             // TODO: Your customization
+        }
+
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+            
+            // Configure static content
+            conventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("static", "wwwroot")
+            );
         }
     }
 }
