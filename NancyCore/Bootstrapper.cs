57a6ee5c90fe1d6d3f64c4bb135d3daf075e8b79
0xFireball <0xFireball@outlook.com>
2017-01-12 19:30:36 -0800

    enable runtime view updates in debug

--- NancyCore/Bootstrapper.cs
+++ NancyCore/Bootstrapper.cs
@@ -1,6 +1,6 @@
 using Nancy;
 using Nancy.Bootstrapper;
-using Nancy.Conventions;
+using Nancy.Configuration;
 using Nancy.TinyIoc;
 
 namespace NancyCore
@@ -12,6 +12,16 @@ namespace NancyCore
             base.ApplicationStartup(container, pipelines);
 
             // TODO: Your customization
+        }
+
+        public override void Configure(INancyEnvironment environment)
+        {
+            base.Configure(environment);
+
+#if DEBUG
+            // If in Debug mode, set some options to speed up development
+            environment.Views(runtimeViewDiscovery: true, runtimeViewUpdates: true);
+#endif
         }
     }
 }
