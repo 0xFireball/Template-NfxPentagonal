0a5be605879bb8dbed1ed283d58a87cda88be62c
0xFireball <0xFireball@outlook.com>
2017-01-23 15:04:42 -0800

    install pentagonal services

--- src/NfxPentagonalCore/Bootstrapper.cs
+++ src/NfxPentagonalCore/Bootstrapper.cs
@@ -1,7 +1,10 @@
+using LiteDB;
 using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.Configuration;
 using Nancy.TinyIoc;
+using Pentagonal;
+using Pentagonal.Auth;
 
 namespace NfxPentagonalCore
 {
@@ -17,6 +20,14 @@ namespace NfxPentagonalCore
         public override void Configure(INancyEnvironment environment)
         {
             base.Configure(environment);
+
+            // Install Pentagonal services
+
+            PentagonalServices.Enable(pipelines, new PentagonalConfiguration());
+            PentagonalAuthenticationServices.Enable(pipelines, new PentagonalAuthConfiguration
+            {
+                Database = new LiteDatabase("omnibus-demo.lidb")
+            });
 
 #if DEBUG
             // If in Debug mode, set some options to speed up development
